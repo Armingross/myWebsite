d69d4fdf0df159c109eed932a41be58b9da5bd29
armin_sarntoul <armingross1981@gmail.com>
2022-12-27 17:33:24 +0100

    need to change parallel scroll effekt cause its scrolling to fast if you are a long way down the page

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -8,8 +8,13 @@ import { useNavigate } from 'react-router-dom';
 export default function Home(){
     <link rel="stylesheet" href="aboutMe.css"/>
 
+    /* let { scrollY } = useScroll();
+    let yUp = useTransform(scrollY, [1700, 10000],["0","-100%"]);
+    let yDown = useTransform(scrollY, [1500, 4000],["0","100%"]); */
     let { scrollYProgress } = useScroll();
-    let y = useTransform(scrollYProgress, [0, 1],["0","-100%"]);
+    let yImg = useTransform(scrollYProgress, [0, 1],["0","-100%"]);
+    let yText = useTransform(scrollYProgress, [0, 1],["0","-70%"]);
+    let yVideoText = useTransform(scrollYProgress, [0, 1],["-50","0%"]);
 
     let navigate = useNavigate();
 
@@ -49,93 +54,93 @@ export default function Home(){
                 {/* First Page */}
                 <div className="page" id="firstPage">
                     {/* Text Box */}
-                    <div id="firstPageTextBox">
+                    <motion.div style={{ y: yText }} id="firstPageTextBox">
                         <p className="text">I'm from Sarnthein</p>
-                    </div>
+                    </motion.div>
                     {/* div which hides overflow */}
-                    <div className="firstPagePictureDiv">
+                    <motion.div style={{ y: yImg }} className="firstPagePictureDiv">
                         {/* Picture */}
                         <img src="img/sarnthein.jpg" alt="picture of Sarnthein"/>
-                    </div>
+                    </motion.div>
                 </div>
 
                 {/* Second Page */}
                 <div className="page" id="secondPage">
                     {/* Text Box */}
-                    <div className="secondPageTextBox">
+                    <motion.div style={{ y: yVideoText }} className="secondPageTextBox">
                         <p className="text">Which is Located</p>
                         <p className="text">in the North</p>
                         <p className="text">of Italy</p>
-                    </div>
+                    </motion.div>
                     {/* Picture Box */}
-                    <div className="secondPageVideoDiv">
+                    <motion.div style={{ y: yImg }} className="secondPageVideoDiv">
                         <video onClick={e => e.target.play()} autoPlay muted>
                             <source src="video/Sarnthein.mp4#svgView(preserveAspectRatio(none))" type="video/mp4"/>
                         </video>
-                    </div>
+                    </motion.div>
                 </div>
 
                 {/* Third Page */}
                 <div className="page" id="thirdPage">
                     <div className="thridTextPictureBox">
-                        <div id="thirdTextBox">
+                        <motion.div style={{ y: yText }} id="thirdTextBox">
                             <p className="text">I like Hiking...</p>
-                        </div>
-                        <div className="thirdPictureTwoDiv">
+                        </motion.div>
+                        <motion.div style={{ y: yImg }} className="thirdPictureTwoDiv">
                             <img src="img/jakobsSpitzePanorama.jpg" alt="picture of a Tent" className="thirdPictureTwo"/>
-                        </div>
+                        </motion.div>
                     </div>
-                    <div className="thirdPictureOneDiv">
+                    <motion.div style={{ y: yImg }} className="thirdPictureOneDiv">
                         <img src="img/hikingTent.jpg" alt="picture of a Tent" className="thirdPictureOne"/>
-                    </div>
+                    </motion.div>
                 </div>
 
                 {/* Traveling page */}
                 <div className="page" id="fourthPage">
-                    <div className="fourthPictures">
+                    <motion.div style={{ y: yImg }} className="fourthPictures">
                         <img src="img/grandCanyon.jpg" alt="picture of me at the Grand Canyon" className="fourthPictureOne"/>
-                    </div>
+                    </motion.div>
                     <div id="FourthTextPictureBox">
-                        <div id="fourthTextBox">
+                        <motion.div style={{ y: yText }} id="fourthTextBox">
                                 <p className="text">Traveling...</p>
-                        </div>
-                        <div className="fourthPictures">
+                        </motion.div>
+                        <motion.div style={{ y: yImg }} className="fourthPictures">
                             <img src="img/newYork.jpg" alt="picture of the New York Skyline" className="fourthPictureTwo"/>
+                        </motion.div>
                     </div>
-                    </div>
-                    <div className="fourthPictures">
+                    <motion.div style={{ y: yImg }} className="fourthPictures">
                         <img src="img/goldenGate.jpg" alt="picture of me in front of the golden Gate Bridge" className="fourthPictureThree"/>
-                    </div>
+                    </motion.div>
                 </div>   
 
                 {/* Fifth page */}
                 <div className="page" id="fifthPage">
-                    <div className="fifthPictures">
+                    <motion.div style={{ y: yImg }} className="fifthPictures">
                         <img src="img/gym.png" alt="picture of me playing volleyball" className="fifthPictureOne"/>
-                    </div>
-                    <div>
+                    </motion.div>
+                    <motion.div style={{ y: yText }}>
                         <p className="text">Sport...</p>
-                    </div>
-                    <div className="fifthPictures">
+                    </motion.div>
+                    <motion.div style={{ y: yImg }} className="fifthPictures">
                         <img src="img/volleyball.jpg" alt="picture of me in the gym" className="fifthPictureTwo"/>
-                    </div>
+                    </motion.div>
                 </div> 
 
                 {/* Sixth page */}
                 <div className="page" id="sixthPage">
-                    <div>
+                    <motion.div>
                         <p className="text">and of course...</p>
-                    </div>
+                    </motion.div>
                 </div>
 
                 {/* Programming page */}
                 <div className="page" id="seventhPage">
-                    <div>
+                    <motion.div style={{ y: yText }} >
                         <p className="text">Programming</p>
-                    </div>
-                    <div className="programmingPicture">
+                    </motion.div>
+                    <motion.div style={{ y: yImg }} className="programmingPicture">
 
-                    </div>
+                    </motion.div>
                     {/* <img src="img/volleyball.jpg" alt="picture of me programming" className="programmingPicture"/> */}
                 </div>
             </motion.div>
