1b81236fa309fab1b85ed4de8d16294a7a4b833a
mermeit-laptop <armingross1981@gmail.com>
2023-01-12 20:10:56 +0100

    switch case and deleted states and changed it witch one state

--- src/components/aboutMe/Scroll.js
+++ src/components/aboutMe/Scroll.js
@@ -1,36 +1,69 @@
-/* scroll edits the boxes id
-and sets them to wherever page the pointer points to and where the boxes should be on that page */
+/* scroll edits the visible boxes to the boxes which pointer points on */
 export function scroll(setBox, pointer){
-    /* here i implement a  */
-    const states = ["prev2", "prev2", "prev2", "prev2", "prev", "active", "next", "next2", "next2", "next2", "next2"]
-    let i;
+    /* "active" box is always the box in the center, prev is the one before and next the one after
+    prev2 is a the one before before which youn cannot see, same patter for next2 */
     switch(pointer){
         case 1:
-            i = 5;
+            /* in this case it is the first box */
+            setBox({
+                box1: "active",
+                box2: "next",
+                box3: "next2",
+                box4: "next2",
+                box5: "next2",
+                box6: "next2"
+            })
             break;
         case 2:
-            i = 4;
+            /* in this case its the second one and so on... */
+            setBox({
+                box1: "prev",
+                box2: "active",
+                box3: "next",
+                box4: "next2",
+                box5: "next2",
+                box6: "next2"
+            })
             break;
         case 3:
-            i = 3;
+            setBox({
+                box1: "prev2",
+                box2: "prev",
+                box3: "active",
+                box4: "next",
+                box5: "next2",
+                box6: "next2"
+            })
             break;
         case 4:
-            i = 2;
+            setBox({
+                box1: "prev2",
+                box2: "prev2",
+                box3: "prev",
+                box4: "active",
+                box5: "next",
+                box6: "next2"
+            })
             break;
         case 5:
-            i = 1;
+            setBox({
+                box1: "prev2",
+                box2: "prev2",
+                box3: "prev2",
+                box4: "prev",
+                box5: "active",
+                box6: "next"
+            })
             break;
         case 6:
-            i = 0;
+            setBox({
+                box1: "prev2",
+                box2: "prev2",
+                box3: "prev2",
+                box4: "prev2",
+                box5: "prev",
+                box6: "active"
+            })
             break;
-    }
-
-    setBox({
-        box1: states[i],
-        box2: states[i + 1],
-        box3: states[i + 2],
-        box4: states[i + 3],
-        box5: states[i + 4],
-        box6: states[i + 5]
-    })    
+        }
 }
