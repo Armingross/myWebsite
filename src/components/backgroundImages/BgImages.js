7b0ab6c1651fd9acbd01e3882f67247a94f7aeae
mermeit-laptop <armingross1981@gmail.com>
2023-01-19 16:54:53 +0100

    bg images responsive

--- src/components/backgroundImages/BgImages.js
+++ src/components/backgroundImages/BgImages.js
@@ -13,28 +13,28 @@ export function BgImages(page){
 
     /* when screen is bigger the 5/2 (high width, low height) */
     if(ratio_5to2){
-        homeBg = "img/bg/backToCam_5to2.jpg";
-        aboutmeBg = "img/bg/onLine_5to2.jpg";
-        contactBg = "img/bg/onRight_5to2.jpg";
+        homeBg = "img/bg/backToCam/backToCam_5to2.jpg";
+        aboutmeBg = "img/bg/onLine/onLine_5to2.jpg";
+        contactBg = "img/bg/onRight/onRight_5to2.jpg";
         console.log("5to2")
     /* when screen is between 5/2 and 7/4 (normal pc display ratio) */
     }else if(ratio_7to4_5to2){
-        homeBg = "img/bg/backToCam_Pc.jpg";
-        aboutmeBg = "img/bg/onLine_Pc.jpg";
-        contactBg = "img/bg/onRight_Pc.jpg";
+        homeBg = "img/bg/backToCam/backToCam_Pc.jpg";
+        aboutmeBg = "img/bg/onLine/onLine_Pc.jpg";
+        contactBg = "img/bg/onRight/onRight_Pc.jpg";
         console.log("5to4")
     }else if(ratio_1to1_7to4){
-        homeBg = "img/bg/backToCam_1to1.jpg";
-        aboutmeBg = "img/bg/onLine_1to1.jpg";
-        contactBg = "img/bg/onRight_1to1.jpg";
+        homeBg = "img/bg/backToCam/backToCam_1to1.jpg";
+        aboutmeBg = "img/bg/onLine/onLine_1to1.jpg";
+        contactBg = "img/bg/onRight/onRight_1to1.jpg";
     }else if(ratio_4to7_1to1){
-        homeBg = "img/bg/backToCamOriginal.jpg";
-        aboutmeBg = "img/bg/onLineOriginal.jpg";
-        contactBg = "img/bg/onRightOriginal.jpg";        
+        homeBg = "img/bg/backToCam/backToCamOriginal.jpg";
+        aboutmeBg = "img/bg/onLine/onLineOriginal.jpg";
+        contactBg = "img/bg/onRight/onRightOriginal.jpg";        
     }else {
-        homeBg = "img/bg/backToCam_Sp.jpg";
-        aboutmeBg = "img/bg/onLine_Sp.jpg";
-        contactBg = "img/bg/onRight_Sp.jpg";         
+        homeBg = "img/bg/backToCam/backToCam_Sp.jpg";
+        aboutmeBg = "img/bg/onLine/onLine_Sp.jpg";
+        contactBg = "img/bg/onRight/onRight_Sp.jpg";         
     }
 
 
